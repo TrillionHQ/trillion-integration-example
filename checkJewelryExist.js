693f06258e89f5ec5d14675be575ad5ba74706d3
Anton Shmigelskiy <bingsa@yandex.ru>
2023-09-05 11:06:26 +0200

    remove cors

--- checkJewelryExist.js
+++ checkJewelryExist.js
@@ -1,7 +1,6 @@
 export async function jewelryExists(jewelryId) {
     const response = await fetch(`https://dashboard.trillion.jewelry/api/trillionwebapp/publication-status/${jewelryId}`, {
         method: "GET",
-        cors: "no-cors",
     })
     const data = await response.json();    
     return data.isPublishedWebSDK;
